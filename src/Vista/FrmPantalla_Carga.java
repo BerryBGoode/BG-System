2ce7fa0acb2c76905ae3fb2e67ca6de5bb425fd7
Ander45Portafol <anderdeco25@gmail.com>
2022-09-07 22:24:48 -0600

    Pantalla de carga

--- src/Vista/FrmPantalla_Carga.java
+++ src/Vista/FrmPantalla_Carga.java
@@ -5,6 +5,21 @@
  */
 package Vista;
 
+import Controlador.ControllerConexion;
+import Controlador.ControllerConfig;
+import Controlador.ControllerP_U_Empresa;
+import Controlador.ControllerP_U_Personal;
+import Controlador.ControllerP_U_Usuarios;
+import static Vista.FrmConfigConection.config;
+import com.sun.awt.AWTUtilities;
+import java.awt.Image;
+import java.awt.Shape;
+import java.awt.Toolkit;
+import java.awt.geom.RoundRectangle2D;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author hp
@@ -14,9 +29,90 @@ public class FrmPantalla_Carga extends javax.swing.JFrame {
     /**
      * Creates new form FrmPantalla_Carga
      */
+    FrmConfigConection frmconexion = new FrmConfigConection();
+    int numerobarra;
+
     public FrmPantalla_Carga() {
         initComponents();
         this.setLocationRelativeTo(null);
+        Shape forma = new RoundRectangle2D.Double(0, 0, this.getBounds().width, this.getBounds().height, 40, 40);
+        AWTUtilities.setWindowShape(this, forma);
+        setIconImage(Logo());
+        correrlo();
+    }
+
+    public Image Logo() {
+        Image retvalue = Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("Recursos_Proyecto/B&G Morado 2.png"));
+        return retvalue;
+    }
+
+    private void barracargar() {
+        for (numerobarra = 0; numerobarra <= 100; numerobarra++) {
+            try {
+                Thread.sleep(20);
+                BarraCarga.setValue(numerobarra);
+                if (numerobarra == 100) {
+                    if (config.checkFile() == true) {
+                        //existe file
+                        config.readFile();
+                        ControllerConfig.setUsername(config.data[0]);
+                        ControllerConfig.setIp(config.data[1]);
+                        ControllerConfig.setHost(config.data[2]);
+                        ControllerConfig.setPassword(config.data[3]);
+
+                        ControllerP_U_Empresa VistapEmpresa = new ControllerP_U_Empresa();
+                        ControllerP_U_Personal PrimerPersonal = new ControllerP_U_Personal();
+                        ControllerP_U_Usuarios primerUsuarios = new ControllerP_U_Usuarios();
+
+                        //lee los valores y los asigna a los attr
+                        if (ControllerConexion.getConnectionModel() != null) {
+                            if (VistapEmpresa.checkEnterprise() == false) {
+                                Bienvenida welcome = new Bienvenida();
+                                welcome.setVisible(true);
+                            } else if (PrimerPersonal.checkcontrollerPersonal() == true && primerUsuarios.checkControllerUsuario() == true && VistapEmpresa.checkEnterprise() == true) {
+                                FrmLogin iniciar = new FrmLogin();
+                                iniciar.setVisible(true);
+                            } else if (VistapEmpresa.checkEnterprise() == true && PrimerPersonal.checkcontrollerPersonal() == false) {
+                                FrmP_U_Personal cargarpersonal = new FrmP_U_Personal();
+                                cargarpersonal.setVisible(true);
+
+                            } else if (PrimerPersonal.checkcontrollerPersonal() == true && primerUsuarios.checkControllerUsuario() == false && VistapEmpresa.checkEnterprise() == true) {
+                                FrmP_U_Personal cargarpersonal = new FrmP_U_Personal();
+                                cargarpersonal.setVisible(true);
+                            }
+                        } /*else if(primerPersonal==true){
+                            FrmP_U_Usuario pu=new FrmP_U_Usuario();
+                            pu.setVisible(true);
+                    }*/ //FrmP_U_Personal pe=new FrmP_U_Personal();
+                        //pe.setVisible(true);
+                        //modificar.
+                        //verificar si existe un usuario 
+                        //si existen, pasar al login 
+                        //si no exsiten la primer uso
+                        else {
+                            JOptionPane.showMessageDialog(null, "No, se logro establercer conexion con la base de datos", "Error de conexion", JOptionPane.ERROR_MESSAGE);
+                            config.setVisible(true);//para volver a config, porque salio algo mal                        
+                            config.deleteFile();
+                        }
+                    } else {
+                        config.setVisible(true);//cuando no exista el archivo
+                    }
+                    this.dispose();
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(FrmPantalla_Carga.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void correrlo() {
+        Thread hilo = new Thread() {
+            @Override
+            public void run() {
+                barracargar();
+            }
+        };
+        hilo.start();
     }
 
     /**
@@ -28,13 +124,40 @@ public class FrmPantalla_Carga extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        panelRound1 = new Controles_Personalizados.Paneles.PanelRound();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        BarraCarga = new Controles_Personalizados.ProgressBarCustom();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+        setPreferredSize(new java.awt.Dimension(900, 500));
 
-        jPanel1.setPreferredSize(new java.awt.Dimension(930, 461));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+        panelRound1.setBackground(new java.awt.Color(42, 36, 56));
+        panelRound1.setRoundBottomLeft(40);
+        panelRound1.setRoundBottomRight(40);
+        panelRound1.setRoundTopLeft(40);
+        panelRound1.setRoundTopRight(40);
+        panelRound1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos_Proyecto/LogoB&GDash.png"))); // NOI18N
+        panelRound1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 80, 130, 140));
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 54)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(219, 216, 227));
+        jLabel2.setText("Beep&Go");
+        panelRound1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 220, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Dialog", 1, 30)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(219, 216, 227));
+        jLabel3.setText("Cargando ...");
+        panelRound1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 410, -1, 40));
+
+        BarraCarga.setBackground(new java.awt.Color(58, 50, 75));
+        panelRound1.add(BarraCarga, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 335, 475, 40));
+
+        getContentPane().add(panelRound1, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -75,6 +198,10 @@ public class FrmPantalla_Carga extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private Controles_Personalizados.ProgressBarCustom BarraCarga;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private Controles_Personalizados.Paneles.PanelRound panelRound1;
     // End of variables declaration//GEN-END:variables
 }
