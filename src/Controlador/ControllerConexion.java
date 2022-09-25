60379e22cc908fd7a78b9c50578c726b3be4e861
itsRoberOOF <83309227+itsRoberOOF@users.noreply.github.com>
2022-09-24 21:59:56 -0600

    Cambios de ayer

--- src/Controlador/ControllerConexion.java
+++ src/Controlador/ControllerConexion.java
@@ -8,15 +8,12 @@ package Controlador;
 import java.sql.Connection;
 import Modelo.ModelConexion;
 /**
- * This class stores and connects the ModelConexion with the view, and makes possible the communication between both
+ *
  * @author danlo and Ferna
  */
 public class ControllerConexion {
     
-    /**
-     * Check the connection data and run it
-     * @return a Connection
-     */
+    
     public static Connection getConnectionModel(){
                 
         return ModelConexion.getConnection();
--- src/Modelo/ModelConexion.java
+++ src/Modelo/ModelConexion.java
@@ -7,7 +7,7 @@ package Modelo;
 
 import java.sql.*;
 /**
- *  This class stores all the sentences necessary for the correct functioning of the database connection
+ *
  * @author danlo
  */
 public class ModelConexion {
@@ -18,90 +18,46 @@ public class ModelConexion {
     private static String host;
     private static String db;
 
-    /**
-     * Gets the username for the database connection
-     * @return a String
-     */
     public static String getUsername() {
         return username;
     }
 
-    /**
-     *  Set the username for the database connection
-     * @param username referring to the username of the user
-     */
     public static void setUsername(String username) {
         ModelConexion.username = username;
     }
 
-    /**
-     * Gets the password for the database connection
-     * @return a String
-     */
     public static String getPassword() {
         return password;
     }
 
-    /**
-     *  Set the password for the database connection
-     * @param password referring to the password 
-     */
     public static void setPassword(String password) {
         ModelConexion.password = password;
     }
 
-    /**
-     * Gets the IP for the database Connection
-     * @return a String
-     */
     public static String getIp() {
         return ip;
     }
 
-    /**
-     *  Set the IP for the database connection
-     * @param ip referring to the IP for the connection
-     */
     public static void setIp(String ip) {
         ModelConexion.ip = ip;
     }
 
-    /**
-     *  Gets the host for the database connection
-     * @return a String
-     */
     public static String getHost() {
         return host;
     }
 
-    /**
-     *  Set the host for the database connection
-     * @param host referring to the host for the connection
-     */
     public static void setHost(String host) {
         ModelConexion.host = host;
     }
 
-    /**
-     *  Gets the database name for the connection
-     * @return a String
-     */
     public static String getDb() {
         return db;
     }
 
-    /**
-     *  Set the database name for the connection
-     * @param db referring to the database name
-     */
     public static void setDb(String db) {
         ModelConexion.db = db;
     }
      
-    /**
-     *  Method that makes the connection to the database
-     * @return a Connection
-     */
     public static Connection getConnection(){        
         setDb("dbBG");
         String url = "jdbc:sqlserver://"+getIp()+":"+getHost()+";"
